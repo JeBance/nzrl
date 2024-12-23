ced291307f67e9282cfcb29253deb32100864c2e
Олег <JeBance>
2024-12-23 15:32:38 +0300

    1.0.4 fixed try catch

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ class nzrl {
 	}
 
 	async requestListener(req, res) {
+		try {
+
 		let nonce = new Date().getTime();
 		res.setHeader('Content-Type', 'application/json');
 		res.setHeader('Access-Control-Allow-Origin', '*');
@@ -44,56 +46,54 @@ class nzrl {
 
 				// handshake
 				if (req.hasOwnProperty('handshake') === true) {
-					try {
-						let senderHash = await getHASH(JSON.stringify(req.handshake), 'md5');
-						if (this.NODE.nodes[senderHash]) throw new Error('The node is already in the list of known nodes.');
-						if (req.handshake.net !== this.CONFIG.net) throw new Error('The node does not match the selected network.');
-						let senderNodeInfo = await this.NODE.getInfo(req.handshake);
-						if (!senderNodeInfo) throw new Error('Failed to get information from node ' + req.handshake.host + ':' + req.handshake.port);
-						if (req.handshake.net !== senderNodeInfo.net) throw new Error();
-						await this.NODE.add({
-							keyID: senderHash,
-							net: req.handshake.net,
-							prot: req.handshake.prot,
-							host: req.handshake.host,
-							port: req.handshake.port,
-							ping: senderNodeInfo.ping
-						});
-					} catch(e) {
-						if (this.CONFIG.log) console.log(e);
-					}
+					let senderHash = await getHASH(JSON.stringify(req.handshake), 'md5');
+					if (this.NODE.nodes[senderHash]) throw new Error('The node is already in the list of known nodes.');
+					if (req.handshake.net !== this.CONFIG.net) throw new Error('The node does not match the selected network.');
+					let senderNodeInfo = await this.NODE.getInfo(req.handshake);
+					if (!senderNodeInfo) throw new Error('Failed to get information from node ' + req.handshake.host + ':' + req.handshake.port);
+					if (req.handshake.net !== senderNodeInfo.net) throw new Error();
+					await this.NODE.add({
+						keyID: senderHash,
+						net: req.handshake.net,
+						prot: req.handshake.prot,
+						host: req.handshake.host,
+						port: req.handshake.port,
+						ping: senderNodeInfo.ping
+					});
 
 				// newMessage
 				} else if (req.hasOwnProperty('newMessage') === true) {
-					try {
-						if (!(await this.MESSAGE.checkMessageStructure(req.newMessage))
-						|| this.MESSAGE.list[req.newMessage.hash] !== undefined) throw new Error();
-						let currentTime = new Date().getTime();
-						let infoNode = await this.NODE.getInfo({
-							prot: req.newMessage.prot,
-							host: req.newMessage.host,
-							port: req.newMessage.port
-						});
-						let inequal = currentTime - (infoNode.time + infoNode.ping);
-						if (!hasPGPstructure(req.newMessage.message)
-						|| (this.MESSAGE.hasExpired(req.newMessage.timestamp))
-						|| !((req.newMessage.timestamp + inequal) < currentTime)
-						|| (infoNode.net !== this.CONFIG.net)) throw new Error();
-						await this.MESSAGE.add(req.newMessage);
-						req.newMessage.prot = this.CONFIG.prot;
-						req.newMessage.host = this.CONFIG.host;
-						req.newMessage.port = this.CONFIG.port;
-						await this.NODE.sendMessageToAll({ newMessage: req.newMessage });
-					} catch(e) {
-						if (this.CONFIG.log) console.log(e);
-					}
+					if (!(await this.MESSAGE.checkMessageStructure(req.newMessage))
+					|| this.MESSAGE.list[req.newMessage.hash] !== undefined) throw new Error();
+					let currentTime = new Date().getTime();
+					let infoNode = await this.NODE.getInfo({
+						prot: req.newMessage.prot,
+						host: req.newMessage.host,
+						port: req.newMessage.port
+					});
+					let inequal = currentTime - (infoNode.time + infoNode.ping);
+					if (!hasPGPstructure(req.newMessage.message)
+					|| (this.MESSAGE.hasExpired(req.newMessage.timestamp))
+					|| !((req.newMessage.timestamp + inequal) < currentTime)
+					|| (infoNode.net !== this.CONFIG.net)) throw new Error();
+					await this.MESSAGE.add(req.newMessage);
+					req.newMessage.prot = this.CONFIG.prot;
+					req.newMessage.host = this.CONFIG.host;
+					req.newMessage.port = this.CONFIG.port;
+					await this.NODE.sendMessageToAll({ newMessage: req.newMessage });
 				}
 
 			// encrypted messages (just save and give)
 			} else if (hasPGPstructure(data)) {
 				res.writeHead(200);
-				try {
-					if (this.MESSAGE.list[hash] !== undefined) throw new Error('The message is already in the list of known messages.');
+				if (this.MESSAGE.list[hash] !== undefined) {
+					let message = await this.MESSAGE.getMessage(hash);
+					res.end(JSON.stringify({
+						result: 'Data successfully received',
+						hash: hash,
+						timestamp: message.timestamp
+					}));
+				} else {
 					res.end(JSON.stringify({
 						result: 'Data successfully received',
 						hash: hash,
@@ -109,14 +109,6 @@ class nzrl {
 					};
 					await this.MESSAGE.add(message);
 					await this.NODE.sendMessageToAll({ newMessage: message });
-				} catch(e) {
-					let message = await this.MESSAGE.getMessage(hash);
-					res.end(JSON.stringify({
-						result: 'Data successfully received',
-						hash: hash,
-						timestamp: message.timestamp
-					}));
-					if (this.CONFIG.log) console.log(e);
 				}
 
 			} else {
@@ -163,14 +155,13 @@ class nzrl {
 					res.end(JSON.stringify(this.MESSAGE.list));
 					break
 				case '/getMessage':
-					try {
-						let message = await this.MESSAGE.getMessage(args[0]);
-						if (!message) throw new Error();
-						res.writeHead(200);
-						res.end(JSON.stringify(message));
-					} catch(e) {
+					let message = await this.MESSAGE.getMessage(args[0]);
+					if (!message) {
 						res.writeHead(404);
 						res.end(JSON.stringify({error:'Resource not found'}));
+					} else {
+						res.writeHead(200);
+						res.end(JSON.stringify(message));
 					}
 					break
 				default:
@@ -178,6 +169,10 @@ class nzrl {
 					res.end(JSON.stringify({error:'Resource not found'}));
 			}
 
+		}
+
+		} catch(e) {
+			if (this.CONFIG.log) console.log(e);
 		}
 
 	}
